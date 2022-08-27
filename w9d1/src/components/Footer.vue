5df5f42f75c1cc74dbaa6498c096e1695a5b85d3
Txell Amigó <txellmgsol@gmail.com>
2022-08-27 17:21:54 +0200

    "extra do a form DONE"

--- w9d1/src/components/Footer.vue
+++ w9d1/src/components/Footer.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>Footer</div>
+  <div>
+    <h2>This is my form:</h2>
+        <input v-model="nameInput" placeholder="Name"/> 
+
+        <input v-model="emailInput" placeholder="e-mail"/> 
+
+    <h2>This is the two-day-data-binding result:</h2>
+        <p>Your name is: {{ nameInput }} </p>
+        <p>Your email is: {{ emailInput }} </p>
+  </div>
 </template>
 
 <script>
+
 export default {
+    data() {
+        return {
+            nameInput: "",
+            emailInput: "",
+        }
+    }
 
 }
 </script>

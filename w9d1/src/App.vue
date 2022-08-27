98473bcbf56ed914fb67d7e392c37a215b31d52a
Txell Amigó <txellmgsol@gmail.com>
2022-08-27 16:27:32 +0200

    iteration 1 and 2 done

---
+++ w9d1/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <body>
+    <header>
+      <NavBar></NavBar>
+    </header>
+    <Footer></Footer>
+  </body>
+</template>
+
+<script>
+
+import Footer from "./components/Footer.vue";
+import NavBar from "./components/NavBar.vue";
+export default {
+    name: 'app',
+    components: {
+    NavBar,
+    Footer,
+    },
+};
+</script>
+
+<style>
+</style>
---
+++ w9d1/src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{mathOperation}}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            mathOperation: 2+2
+            
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>

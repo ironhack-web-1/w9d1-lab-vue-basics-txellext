2ec7966380a2d58fdd6c6fc7c26af67c6dc72cfd
Txell Amigó <txellmgsol@gmail.com>
2022-09-01 21:26:25 +0200

    "formulario con v-model"

--- w9d1/src/App.vue
+++ w9d1/src/App.vue
@@ -3,36 +3,65 @@
     <header>
       <NavBar></NavBar>
     </header>
-    <Footer @updateEmail="updateEmail" @update="updateName" :nameInput="nameInput" :emailInput="emailInput"></Footer>
+    <Footer
+      @updateEmail="updateEmail"
+      @update="updateName"
+      :nameInput="nameInput"
+      :emailInput="emailInput"
+    ></Footer>
     <BodyConsent :nameInput="nameInput" :emailInput="emailInput"></BodyConsent>
   </div>
-  </template>
+
+  <hr />
+  <hr />
+  <h2>Formulario nuevo con v-model</h2>
+  <form action="/some-url" method="get">
+    <NewForm v-model="name" placeholder="Name"/>
+    <NewForm v-model="surname" placeholder="Surname"/>
+    <NewForm v-model="number" type="number" placeholder="Number"/>
+    <NewForm type="submit" value="Send"/>
+  </form>
+
+  <div>
+    <h2>El padre se entera?</h2>
+    <div>Name: {{name}}</div>
+    <div>Surname: {{surname}}</div>
+    <div>Number: {{number}}</div>
+  </div>
+</template>
+
 
 <script>
 import Footer from "./components/Footer.vue";
 import NavBar from "./components/NavBar.vue";
 import BodyConsent from "./components/BodyConsent.vue";
+import NewForm from "./components/NewForm.vue";
+
 export default {
   name: "app",
   components: {
     NavBar,
     Footer,
     BodyConsent,
+    NewForm,
   },
   data() {
-        return {
-            nameInput: "",
-            emailInput: "",
-        }
+    return {
+      nameInput: "",
+      emailInput: "",
+      name: null,
+      surname: null,
+      number: null,
+    };
+  },
+  methods: {
+    updateName(value) {
+      this.nameInput = value;
     },
-    methods: {
-      updateName(value) {
-        this.nameInput = value;
-      },
-      updateEmail(value){
-          this.emailInput = value;
-        }
-    }
+    updateEmail(value) {
+      this.emailInput = value;
+    },
+  },
 };
 </script>
 <style></style>
---
+++ w9d1/src/components/NewForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input
+    :placeholder="placeholder"
+    :type="type"
+    :name="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        placeholder: {
+            type: String,
+            default: "text",
+        },
+        type: {
+            default: "text"
+        },
+        modelValue: {
+            type: String,
+            required: true,
+        },
+
+
+    }
+}
+</script>
+
+<style>
+
+</style>
